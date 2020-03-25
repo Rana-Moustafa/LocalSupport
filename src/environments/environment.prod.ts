cd53a0ca72108a653749393e8cd9e06785871169
Rana-Moustafa <rana_moustafa@outlook.com>
2020-03-25 17:10:34 +0200

    deploy staging

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   // baseURL: 'https://belocalhero.com/wordpress-api/staging',
   // liveURL: 'https://belocalhero.com/frontend-staging'
-  baseURL:  'https://belocalhero.com/wordpress-api/live',
-  liveURL: 'https://belocalhero.com'
+  baseURL: 'http://belocalhero.com/wordpress-api/staging/',
+  liveURL: 'http://belocalhero.com/frontend-staging'
 };
