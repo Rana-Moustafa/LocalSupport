6b6844c35c7923a5c33568a4414290d0c57aa122
Rana-Moustafa <rana_moustafa@outlook.com>
2020-03-25 17:17:54 +0200

    updates staging

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // baseURL: 'https://belocalhero.com/wordpress-api/staging',
-  // liveURL: 'https://belocalhero.com/frontend-staging'
-  baseURL: 'http://belocalhero.com/wordpress-api/staging/',
-  liveURL: 'http://belocalhero.com/frontend-staging'
+  baseURL: 'https://belocalhero.com/wordpress-api/staging',
+  liveURL: 'https://belocalhero.com/frontend-staging'
+  // baseURL: 'https://belocalhero.com/wordpress-api/live',
+  // liveURL: 'https://belocalhero.com'
 };
