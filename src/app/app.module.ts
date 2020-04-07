e48a94fd3d2badb14264008246ab779f7c8c3ab1
Rana-Moustafa <rana_moustafa@outlook.com>
2020-04-07 17:26:43 +0200

    change map key

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -216,7 +216,7 @@ export const ngxDropTargetOptions: DropTargetOptions = {
     NgxUploadModule.forRoot(ngxDropTargetOptions),
     // Swal.forRoot(),
     AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyA1Ml1nNKfi1rOebQI0OB3fEgF3zYvPTkA',
+      apiKey: 'AIzaSyAyWxJuiFnfiMbs4WR1iablJIro0n8M-FE',
       libraries: ['places']
     }),
     AgmJsMarkerClustererModule,
